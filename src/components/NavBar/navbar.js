37b1b6e13eaebf4c78e00378232a8f9f4ac6c297
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-31 23:55:21 +0530

    Link added

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import  "./navbar.css";
-import logo from "../../assets/"
-
+import React from "react";
+import "./navbar.css";
+// import logo from '../../assets/logo.png';
+import {Link} from 'react-scroll';
 const Navbar = () => {
   return (
-   <nav className='navbar'>
-    <img src="logo" alt=''/>
-    <div className='desktopMenu'>
-
-    </div>
-    <button className='desktopMenuBtn'>
-<img src='' alt='' className='desktopMenuImg'/>Contact Me
-    </button>
-   </nav>
-
-
-  )
-}
+    <nav className="navbar">
+      <img src="" alt="" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" />
+        Contact Me
+      </button>
+    </nav>
+  );
+};
 
 export default Navbar;
