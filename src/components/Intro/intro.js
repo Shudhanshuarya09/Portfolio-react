03dbdec25746d3e7faf6fc01a1db98c9903c8b52
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-11 16:41:33 +0530

    Intro section html code completed

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import './intro.css'
+import './intro.css';
+import {Link} from 'react-scroll';
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+   <section id='intro'>
+    <div className='introContent'>
+      <span className='hello'>Hello,</span>
+      <br/>
+      <span className='introtext'>I'm <span className='introName'>Shudhanshu Arya</span> <br/> Web Developer</span>
+      <p className='intoPara'>I am a passionate web developer with full of skills and ambitions looking for the great oppurtinity</p>
+      <Link><button className='btn'><img src='/assets/hireme.png' alt=''/> Hire Me </button></Link>
+      
+
+    </div>
+    {/* <img src='/Images/my.jpg' alt=''/> */}
+
+   </section>
+
   )
 }
 
