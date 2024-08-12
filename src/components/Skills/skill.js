4bbe5bef9a3fd7dee1208a3a1d772a64b557131c
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-12 16:38:36 +0530

    Images added in skills section

--- src/components/Skills/skill.js
+++ src/components/Skills/skill.js
@@ -4,38 +4,38 @@ import "./skill.css";
 const Skill = () => {
   return (
     <section id="skills">
-      <span className="skillTitle"> My Skill's</span>
+      <span className="skillTitle"> What I Do</span>
       {/* <span className='skillDesc'>lorem51</span> */}
       <div className="skillBars">
         <div className="skillBar">
-          <img src="" alt="" className="skillBarImg"/>
+          <img src="/Images/stack.avif" alt="" className="skillBarImg"/>
           <div className="skillBarText">
-            <h2></h2>
-            <p></p>
+            <h2>MERN Stack Developer</h2>
+            <p>Basic structure of website</p>
           </div>
         </div>
 
         <div className="skillBar">
-          <img src="" alt="" className="skillBarImg"/>
+          <img src="/Images/content.jpeg" alt="" className="skillBarImg"/>
           <div className="skillBarText">
-            <h2></h2>
-            <p></p>
+            <h2>Content Writer</h2>
+            <p>Presntation of the website</p>
           </div>
         </div>
 
         <div className="skillBar">
-          <img src="" alt="" className="skillBarImg"/>
+          <img src="/Images/editing.jpeg" alt="" className="skillBarImg"/>
           <div className="skillBarText">
-            <h2></h2>
-            <p></p>
+            <h2>Video Editior</h2>
+            <p>Brain of the Websirte</p>
           </div>
         </div>
 
         <div className="skillBar">
-          <img src="" alt="" className="skillBarImg"/>
+          <img src="/Images/competative.jpeg" alt="" className="skillBarImg"/>
           <div className="skillBarText">
-            <h2></h2>
-            <p></p>
+            <h2>CP</h2>
+            <p>Competative programming enahnces my problerm solving ability</p>
           </div>
         </div>
 
