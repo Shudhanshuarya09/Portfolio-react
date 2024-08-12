634cda4522c255630744a7413dd019efcc74a686
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-13 00:10:53 +0530

    Work section half complete

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import NavBar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import About from "./components/About/about";
 import Skills from "./components/Skills/skill";
+import Works from "./components/Works/work";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Intro></Intro>
-
       <About></About>
       <Skills></Skills>
+      <Works></Works>
     </div>
   );
 }
---
+++ src/components/Works/work.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./work.css";
+
+const Work = () => {
+  return (
+    <section id="works">
+      <h1 className="worksTitle">My Portfolio</h1>
+      <span className="worksDesc">lorem ipsum51</span>
+      <div className="worksImages">
+        <img src="/assets/portfolio-1.png" alt="" className="worksImage" />
+        <img src="/assets/portfolio-2.png" alt="" className="worksImage" />
+        <img src="/assets/portfolio-3.png" alt="" className="worksImage" />
+        <img src="/assets/portfolio-4.png" alt="" className="worksImage" />
+        <img src="/assets/portfolio-5.png" alt="" className="worksImage" />
+        <img src="/assets/portfolio-6.png" alt="" className="worksImage" />
+      </div>
+      <button className="worksBtn">See More</button>
+    </section>
+  );
+};
+
+export default Work;
