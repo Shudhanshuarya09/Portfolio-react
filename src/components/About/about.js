d048968e6df958ce5089441d8ade74521a220aa7
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-14 01:42:22 +0530

    About section finalize

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -5,11 +5,50 @@ const About = () => {
   return (
     <section id="about">
       <div className="aboutImgDiv">
-        <img  src="/Images/about.jpg" alt="" className="aboutImg"/>
+        <img  src="/Images/mee.jpeg" alt="" className="aboutImg"/>
+       
       </div>
       <div>
-        <h1 className="aboutHeading">About Me</h1>
-        <p className="aboutPara"> lorem51</p>
+      <h1 className="aboutHeading">About Me</h1>
+        <p className="aboutPara"> 
+      <div className="top">
+      ● Pursuing a Bachelor of Engineering in Computer Science and Engineering.<br/>
+
+● A-grade CBSE graduate with a strong academic background.<br/>
+      </div>
+
+<span className="title">● Skills:<br/></span>
+
+<div className="text">
+○ Proficient in web development using HTML, CSS, JavaScript, and frameworks.<br/>
+○ Strong knowledge in Data Structures and Algorithms (DSA).<br/>
+○ Skilled in video editing and content writing for YouTube.<br/>
+</div>
+<span className="title">● Experience:<br/></span>
+
+<div className="text"> 
+○ Developed multiple web applications with front-end and back-end expertise.<br/>
+○ Managed and produced content for a YouTube channel.<br/>
+</div>
+<span className="title">● Passion:<br/></span>
+
+<div className="text">
+○ Committed to continuous learning and improvement.<br/>
+○ Enthusiastic about open-source contributions and coding challenges.<br/>
+</div>
+<span className="title">● Additional Attributes:<br/></span>
+
+<div className="text">
+○ Strong problem-solving, communication, and teamwork skills.
+</div>
+
+
+
+
+
+
+
+ </p>
       </div>
     </section>
   );
