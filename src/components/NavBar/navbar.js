bc6ef102b69d6b3fdf95924bd91b8b1f0e1dc79b
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-18 18:03:40 +0530

    mob menu part completed

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,13 @@
+import  {useState} from 'react';
 import React from "react";
 import "./navbar.css";
 import {Link} from 'react-scroll';
 
 
+
 const Navbar = () => {
+const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
      <img src="/Images/logooo.png" alt="" className="logo" />
@@ -25,6 +29,18 @@ const Navbar = () => {
         <img src="/assets/Contact.png" alt="" className="desktopMenuImg" />
         Contact Me
       </button>
+
+
+      {/* bugMenu */}
+
+      <img src="/assets/menu.png" alt="Menu" className="mobMenu" onClick={()=>setShowMenu(!showMenu)}/>
+      <div className="navMenu" style={{display:showMenu? 'flex' : 'none'}}>
+        <Link activeClass='active' to="intro" spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>Home</Link>
+        <Link activeClass='active' to="works" spy={true} smooth={true} offset={-50} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>Projects</Link>
+       <Link activeClass='active' to="about" spy={true} smooth={true} offset={-70} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>About</Link>
+       <Link activeClass='active' to="experience" spy={true} smooth={true} offset={-50} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>Experience</Link>
+       <Link activeClass='active' to="contact" spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>Contact</Link>
+      </div> 
     </nav>
   );
 };
