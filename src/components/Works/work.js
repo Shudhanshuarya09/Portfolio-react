4e048532a4b758447e66fc51c45b6040d8ad763d
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-08-31 18:52:49 +0530

    modifications

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -7,12 +7,12 @@ const Work = () => {
       <h1 className="worksTitle">My Projects</h1>
       <span className="worksDesc"></span>
       <div className="worksImages">
-        <img src="/assets/portfolio-1.png" alt="" className="worksImage" />
-        <img src="/assets/portfolio-2.png" alt="" className="worksImage" />
+        <img src="/Images/P1.png" alt="" className="worksImage" />
+        <img src="/Images/P2.png" alt="" className="worksImage" />
         <img src="/assets/portfolio-3.png" alt="" className="worksImage" />
         <img src="/assets/portfolio-4.png" alt="" className="worksImage" />
         <img src="/assets/portfolio-5.png" alt="" className="worksImage" />
-        <img src="/assets/portfolio-6.png" alt="" className="worksImage" />
+        {/* <img src="/assets/portfolio-6.png" alt="" className="worksImage" /> */}
       </div>
       <button className="worksBtn">See More</button>
     </section>
