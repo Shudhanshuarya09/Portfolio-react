233005fa6c07f8508e35f504de0a3bd11974f0af
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-05 18:38:15 +0530

    image slider

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -6,14 +6,31 @@ const Work = () => {
     <section id="works">
       <h1 className="worksTitle">My Projects</h1>
       <span className="worksDesc"></span>
-      <div className="worksImages">
+      {/* <div className="worksImages">
+      <img src="/Images/P2.png" alt="" className="worksImage" />
         <img src="/Images/P1.png" alt="" className="worksImage" />
-        <img src="/Images/P2.png" alt="" className="worksImage" />
+        
         <img src="/assets/portfolio-3.png" alt="" className="worksImage" />
         <img src="/assets/portfolio-4.png" alt="" className="worksImage" />
         <img src="/assets/portfolio-5.png" alt="" className="worksImage" />
-        {/* <img src="/assets/portfolio-6.png" alt="" className="worksImage" /> */}
+       
+      </div> */}
+
+      <div className="container">
+        <div className="card-wrapper">
+          <ul className="card-list">
+            <li className="card-item">
+              <a href="#" className="card-link">
+                <img src="Images/P2.png" alt="card-image" className="card-image"/>
+                <p className="badge">MERN Stack</p>
+                <h2 className="card-title"> Techfest'24 SLIET Website </h2>
+                <button className="card-button">Arrow_Forward</button>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
+
       <button className="worksBtn">See More</button>
     </section>
   );
