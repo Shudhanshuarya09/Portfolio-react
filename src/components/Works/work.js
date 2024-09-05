8ad1741dd3920f8660c5ed1d5b0abdc4708f8329
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-05 23:02:42 +0530

    structure ready

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -3,36 +3,64 @@ import "./work.css";
 
 const Work = () => {
   return (
-    <section id="works">
-      <h1 className="worksTitle">My Projects</h1>
-      <span className="worksDesc"></span>
-      {/* <div className="worksImages">
-      <img src="/Images/P2.png" alt="" className="worksImage" />
-        <img src="/Images/P1.png" alt="" className="worksImage" />
+    // <section id="works">
+    //   <h1 className="worksTitle">My Projects</h1>
+    //   <span className="worksDesc"></span>
+    //   <div className="worksImages">
+    //   <img src="/Images/P2.png" alt="" className="worksImage" />
+    //     <img src="/Images/P1.png" alt="" className="worksImage" />
         
-        <img src="/assets/portfolio-3.png" alt="" className="worksImage" />
-        <img src="/assets/portfolio-4.png" alt="" className="worksImage" />
-        <img src="/assets/portfolio-5.png" alt="" className="worksImage" />
+    //     <img src="/Images/P2.png" alt="" className="worksImage" />
+    //     <img src="/Images/P1.png" alt="" className="worksImage" />
+    //     <img src="/Images/P2.png" alt="" className="worksImage" />
        
-      </div> */}
+    //   </div>
 
-      <div className="container">
+      
+
+    //   <button className="worksBtn">See More</button>
+    // </section>
+
+    
+      <div id="works">
         <div className="card-wrapper">
           <ul className="card-list">
+           
+
             <li className="card-item">
               <a href="#" className="card-link">
-                <img src="Images/P2.png" alt="card-image" className="card-image"/>
+                <img src="/Images/P1.png" alt="card-image" className="card-image"/>
                 <p className="badge">MERN Stack</p>
                 <h2 className="card-title"> Techfest'24 SLIET Website </h2>
                 <button className="card-button">Arrow_Forward</button>
               </a>
             </li>
+            <li className="card-item">
+              <a href="#" className="card-link">
+                <img src="/Images/P2.png" alt="card-image" className="card-image"/>
+                <p className="badge">MERN Stack</p>
+                <h2 className="card-title"> Techfest'24 SLIET Website </h2>
+                <button className="card-button">Arrow_Forward</button>
+              </a>
+            </li>
+
+            <li className="card-item">
+              <a href="#" className="card-link">
+                <img src="/Images/P2.png" alt="card-image" className="card-image"/>
+                <p className="badge">MERN Stack</p>
+                <h2 className="card-title"> Techfest'24 SLIET Website </h2>
+                <button className="card-button">Arrow_Forward</button>
+              </a>
+            </li>
+
+            
+
+            
+           
           </ul>
         </div>
       </div>
-
-      <button className="worksBtn">See More</button>
-    </section>
+   
   );
 };
 
