c9a25691b9803024ed8567f2f966b5835d80c42a
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-09 23:19:54 +0530

    modification

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -44,7 +44,7 @@ const Work = () => {
               />
               <p className="badge">MERN Stack</p>
               <h2 className="card-title"> Techfest'24 SLIET Weebsite </h2>
-              <button className="card-button">Arow</button>
+              <button className="card-button">Arrow</button>
             </a>
           </li>
           <li className="card-item">
