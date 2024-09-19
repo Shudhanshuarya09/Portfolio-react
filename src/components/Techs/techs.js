41ef10f10152353795f04bea7b17887c47f9dd80
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-19 16:40:56 +0530

    techs section complete

--- src/components/Techs/techs.js
+++ src/components/Techs/techs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./techs.css";
 
 export const Techs = () => {
   return (
@@ -7,61 +8,77 @@ export const Techs = () => {
 
       <div className="outer">
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/html-5.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para">HTML</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/css-3.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para"> Tailwind-CSS</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/js.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para">Javascript</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/react.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para">React</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/node.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para">NodeJS</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/mongo.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para">Mongodb</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/base.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para"> SQL</p>
         </div>
 
         <div className="cards">
-          <div className="image">
-            <img alt="" src="" />
+          <div className="images">
+            <img alt="" src="/Images/git.png" className="image" />
           </div>
-          <p>Lorum Epsum</p>
+          <p className="para">Git HUb</p>
+        </div>
+
+        <div className="cards">
+          <div className="images">
+            <img alt="" src="/Images/cp.png" className="image" />
+          </div>
+          <p className="para">C++</p>
+        </div>
+
+        <div className="cards">
+          <div className="images">
+            <img alt="" src="/Images/c.png" className="image" />
+          </div>
+          <p className="para">C language</p>
         </div>
       </div>
+
+      
     </section>
   );
 };
