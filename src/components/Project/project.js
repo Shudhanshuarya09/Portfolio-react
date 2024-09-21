71e7124ea64776470ef06d2e8a31f748620db9bc
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-21 13:32:04 +0530

    slider setting

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./project.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Project = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <section id="Project">
       <h4 className="titlee">My Projects</h4>
       <div className="outer">
         <div className="inner">
+        <Slider {...settings}>
           {data.map((d) => (
             <div className="card">
               <div className="imagesss">
@@ -20,6 +31,7 @@ export const Project = () => {
               </div>
             </div>
           ))}
+           </Slider>
         </div>
       </div>
     </section>
@@ -28,12 +40,12 @@ export const Project = () => {
 
 const data = [
   {
-    name: "sharpcode",
+    name: "Sharpcode",
     img: "./Images/p1.png",
     review: "Lorem ipsum 50 sdjgisdsidoufgsduisdbkfjsgduiwefjwekgeuifemhfiusfnashfyofwenfwefgweiegifleuftgf",
   },
   {
-    name: "techfest sliet",
+    name: "Techfest SLIET",
     img: "./Images/p2.png",
     review: "Lorem ipsum 50",
   },
